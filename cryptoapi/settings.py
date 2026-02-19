0064a03f8133da371aa2d94d80002db7ca194ec8
salumsembe <salumrashidjr@gmail.com>
2026-02-19 11:49:38 +0300

    new payment database

--- cryptoapi/settings.py
+++ cryptoapi/settings.py
@@ -79,13 +79,22 @@ WSGI_APPLICATION = 'cryptoapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crypto',
+        'USER': 'root',
+        'PASSWORD': '3102001Salum',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
